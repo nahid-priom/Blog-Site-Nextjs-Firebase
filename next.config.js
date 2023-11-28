bf0b34ba1408e23fa8062bdf963f64bf11b3c670
nahid-priom <nahid.priom.06@gmail.com>
2023-11-29 00:54:02 +0600

    next config output remove

--- next.config.js
+++ next.config.js
@@ -2,21 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Remove the "output" configuration and use the new "exportPathMap"
-  // output: "export",
   images: {
     unoptimized: true,
-  },
-  // Add the exportPathMap function
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      // Specify your export paths here
-      '/': { page: '/' },
-      // Add other paths as needed
-    };
   },
 };
 
