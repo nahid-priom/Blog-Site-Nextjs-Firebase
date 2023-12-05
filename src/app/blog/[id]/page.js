4ee7741c0c77cc11f71a1415dea4ed945ae81e30
nahid-priom <nahid.priom.06@gmail.com>
2023-12-05 22:56:25 +0600

    Paragraph bug fix

--- src/app/blog/[id]/page.js
+++ src/app/blog/[id]/page.js
@@ -1,8 +1,7 @@
-// pages/blog/[id].js
 "use client";
 import { useParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useRef, useState } from "react";
+import { addDoc, collection, doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "../../firebase-config";
 import DOMPurify from "dompurify";
 
@@ -10,6 +9,11 @@ const SingleBlogPost = () => {
   const { id } = useParams();
 
   const [post, setPost] = useState(null);
+  const [readingTime, setReadingTime] = useState(0); // Initial reading time in minutes
+  console.log(readingTime)
+  const [isReading, setIsReading] = useState(false);
+  const readingTimeRef = useRef(readingTime);
+
 
   useEffect(() => {
     const fetchPost = async () => {
@@ -34,12 +38,74 @@ const SingleBlogPost = () => {
     fetchPost();
   }, [id]);
 
+  useEffect(() => {
+    readingTimeRef.current = readingTime;
+  }, [readingTime]);
+  
+  const handleScroll = () => {
+    const postContentElement = document.getElementById("post-content");
+
+    if (postContentElement) {
+      const scrollPosition = window.scrollY;
+      const postHeight = postContentElement.offsetHeight;
+      const windowHeight = window.innerHeight;
+
+      const scrollPercentage =
+        (scrollPosition / (postHeight - windowHeight)) * 100;
+
+      setIsReading(scrollPercentage < 100);
+    }
+  };
+
+  const updateReadingTimeInFirestore = async () => {
+    try {
+      // Reference the specific document using the id
+      const postDocRef = doc(db, "BlogPosts", id);
+  
+      // Get the latest readingTime value
+      const latestReadingTime = readingTimeRef.current;
+  
+      // Update the document with the new readingTime
+      await updateDoc(postDocRef, {
+        readingTime: latestReadingTime,
+      });
+  
+      console.log("Reading time updated for post with ID:", id);
+    } catch (error) {
+      console.error("Error updating reading time:", error);
+    }
+  };
+
+  const updateReadingTime = () => {
+    if (isReading) {
+      // Adjust the reading time based on your own calculations
+      setReadingTime((prevReadingTime) => prevReadingTime + 0.2);
+    } else {
+      updateReadingTimeInFirestore();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    const intervalId = setInterval(updateReadingTime, 10000); // Update every 10 seconds
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+      clearInterval(intervalId);
+    };
+  }, [isReading, readingTime, id, updateReadingTimeInFirestore]);
+
   if (!post) {
     // Handle loading state or post not found
-    return <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl">Loading...</div>;
+    return (
+      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl">
+        Loading...
+      </div>
+    );
   }
 
   const sanitizedContent = DOMPurify.sanitize(post.contentData);
+
   return (
     <div className="max-w-[680px] md:mx-auto my-6 md:my-12 mx-4">
       <h1 className="text-2xl md:text-4xl font-bold mb-4">{post.title}</h1>
@@ -50,17 +116,14 @@ const SingleBlogPost = () => {
           className="w-12 h-12 rounded-full mr-2"
         />
         <div className="flex flex-col items-baseline">
-          <p className="text-base font-bold pr-2">
-            {post.authorName}
-          </p>
+          <p className="text-base font-bold pr-2">{post.authorName}</p>
           <p className="text-sm text-gray-900">
             {post.createdAt.toDate().toLocaleString()}
           </p>
-
         </div>
-          <p className="ml-2">
-             5 min read 
-          </p>
+        <p id="post-content" className="ml-2">
+          {Math.ceil(readingTime)} min read
+        </p>
       </div>
       <div dangerouslySetInnerHTML={{ __html: sanitizedContent }} />
       {/* Add other post details as needed */}
